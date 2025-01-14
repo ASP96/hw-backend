65358248c7bf4335d324bc9f5b0ea6b6a3ae1493
Sergey Pribylskiy <asp@s96.ru>
2025-01-15 00:38:55 +0500

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use App\Http\Resources\VendorResource;
+use Carbon\CarbonInterval;
+use Illuminate\Database\Connection;
+use Illuminate\Foundation\Http\Kernel;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Model;
 
@@ -26,5 +30,30 @@ class AppServiceProvider extends ServiceProvider
         Model::shouldBeStrict(!app()->isProduction());
         // вызвать Exception, если идет сохрание поля, не указанного в модели $fillable = [....];
         Model::preventSilentlyDiscardingAttributes(!app()->isProduction());
+
+        if (app()->isProduction()) {
+            DB::whenQueryingForLongerThan(CarbonInterval::second(5), function(Connection $connection) {
+                logger()
+                    ->channel("local")
+                    ->debug('whenQueryingForLongerThan: '. $connection->totalQueryDuration());
+            });
+
+            DB::listen(function ($query) {
+                if ($query->time > 100) {
+                    logger()
+                        ->channel("local")
+                        ->debug('whenQueryingForLongerThan: ' . $query->sql, $query->bindings);
+                }
+            });
+
+            app(Kernel::class)->whenRequestLifecycleIsLongerThan(
+                CarbonInterval::second(4),
+                function() {
+                    logger()
+                        ->channel("local")
+                        ->debug('whenRequestLifecycleIsLongerThan: '. request()->url());
+                }
+            );
+        }
     }
 }
